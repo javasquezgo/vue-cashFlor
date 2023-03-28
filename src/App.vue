ef4ff322d5bf5a9d9debf75ecfb0858ad8d04228
Alejandro Vasquez <josejaguer12@gmail.com>
2023-03-27 22:10:33 -0600

    Se inicio un nuevo proceso o clase como guste llamar, ahorita con asyncDefineComponent se esta creando una manera que se vea un splashscreen antes de ver la verdadera información del sistema

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import SplashScreen from "./components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
+
+const Home = defineAsyncComponent(() => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(import("./components/Home.vue"));
+    }, 2500);
+  });
+});
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <Home></Home>
+    </template>
+    <template #fallback>
+      <splash-screen></splash-screen>
+    </template>
+  </Suspense>
+</template>
+
+<style scoped>
+</style>
